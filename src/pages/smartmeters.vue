073ad152ec5e3fe671498e576f55451e65455617
Ibrahim Nkya <ibra.isn@gmail.com>
2022-09-16 14:29:08 +0300

    Initial Phase - Corporate Website 60%

---
+++ src/pages/smartmeters.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <NavTwo />
+    <PageHeader title="Our Services" />
+    <ServicePage />
+    <FooterTwo />
+  </div>
+</template>
+
+<script>
+
+  import FooterTwo from "../components/FooterTwo";
+  import PageHeader from "../components/PageHeader";
+  import ServicePage from "../components/ServicePage";
+  import NavTwo from "../components/NavTwo";
+  export default {
+    components: {
+      ServicePage,
+      PageHeader,
+      FooterTwo,
+      NavTwo
+    },
+    head(){
+      return {
+        title: "AFAK Tanzania | Our Services"
+      }
+    }
+  }
+</script>
